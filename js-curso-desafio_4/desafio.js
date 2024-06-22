6891411778e38f81849d1f80f73fb8e6e6c53957
Deborah Ferreira <debahferreira2024@gmail.com>
2024-06-22 20:20:25 -0300

    alterar nomes

--- js-curso-desafio_4/desafio.js
+++ js-curso-desafio_4/desafio.js
@@ -4,5 +4,5 @@ let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
 linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
 console.log(linguagensDeProgramacao);
 
-let listaNomes = ['Deborah', 'Igor', 'Marcelo'];
+let listaNomes = ['Deborah', 'Chantilly', 'Eliane'];
 console.log(listaNomes[0]);
